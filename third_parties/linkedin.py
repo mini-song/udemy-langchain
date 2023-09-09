e1d7dcbd2725e6a922127fd6436e5042c13ee8f1
mini-song <ajohn96@naver.com>
2023-09-09 19:18:57 +0900

    added linkedIn thirdparty package and suspend this project

---
+++ third_parties/linkedin.py
@@ -0,0 +1,17 @@
+import os
+import requests
+
+
+
+def scrape_linkedin_profile(linkedin_profile_url:str):
+    """scrape information from LinkedIn profiles,
+    Manually scrape the information from the LinkendIn profile"""
+    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+    header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+
+
+    response = requests.get(
+        api_endpoint, params={"url":linkedin_profile_url} , headers = header_dic
+    )
+
+    return response
